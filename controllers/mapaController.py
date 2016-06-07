328308dcbb224b12dcd291ab4c9bb23e9ff3befa
portisk8 <porti.sk8@gmail.com>
2016-06-06 22:27:19 -0300

    Augusto

---
+++ controllers/mapaController.py
@@ -0,0 +1,29 @@
+from gluon.serializers import loads_json
+
+def index():
+	return dict()
+
+def getMarkers():
+	"""	-nombre
+		-tipo_Estable', ('kiosco', 'restaurante', 'otro')
+		-descripcion
+		-telefono
+		-direccion
+		-lat
+		-lon
+		-pdf
+	"""
+	places = []
+	rows = db(db.place.id !=None).select()
+	for row in rows:
+		place = {
+			'lat': row.lat,
+			'lng': row.lon,
+			'title': row.name,
+			'infowindow': { 'content': "<h1>" + row.nombre +"</h1><p> Tel: "+ row.telefono +"</p><p>Direccion: "+row.direccion+"</p><p>"+row.descripcion+"</p>" }
+			'pdf':row.pdf,
+			'tipo':row.tipo_Estable,
+			'nombre':row.nombre,
+		}
+		places.append(place)
+	return response.json(places)
