be57c13ea1c74d57a12568b27bd1a787ce254cac
nkarg <r.cadenas.edu@gmai.com>
2016-06-08 13:09:59 -0300

    Eduardo

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,26 @@
 function btn1Click(){
-	$("#cardImage").attr("src", "../static/images/card.png");
-	$("#cardTitle").text("Title 1");
-	$("#cardLink").text("Link 1");
-	$("#cardContent").text("Content 1");
-	
+	$("#cardImage").attr("src", "../static/images/urunday1.jpg");
+	$("#cardTitle").text('Fundación Urunday');
+	$("#cardWLink").attr("href", "http://www.fundacionurunday.org/");
+	$("#cardLink").text("+Info Fundación");
+	$("#cardHtml").html("<p>Los primeros monumentos urbanos(1920) fueron erigidos por la colectividad italiana, los cuales tuvieron el caracter recordatorio de las gestas inmigrantes. A partir de 1961, la historia local comenzo a concretarse con monumentos dedicados a proceres argentinos. En 1961, El Fogon de los Arrieros lanzo un 'Plan de Embellecimiento', donde primaba,en cierta forma crear una 'Ciudad-Museo'. Desde el Primer Concurso de Escultura en Madera en la Plaza Central, en 1988, hasta las Bienales internacionales en la actualidad, El Gobierno de la Provincia del Chaco y la Fundación Urunday trabajan incansablemente para hacer de cada Bienal una verdadera celebración de la identidad de los chaqueños. La Bienal 2016 convoca a once artistas consagrados internacionalmente, representando a distintos paises del mundo, quienes realizaran a cielo abierto, una escultura inspirandoseen el equilibrio. El material a trabajar es el marmol,con el concepto de 'Equilibrium' reconocido como  el cobijo de la armonia, el balance.</p>");
+	$("#cardContent").text("Este es el contenido de la Fundación");
 }
 
 function btn2Click(){
 	$("#cardImage").attr("src", "../static/images/card.png");
-	$("#cardTitle").text("Title 2");
-	$("#cardLink").text("Link 2");
-	$("#cardContent").text("Content 2");
+	$("#cardTitle").text("Bienal 2016");
+	$("#cardWLink").attr("href", "http://www.bienaldelchaco.org/");
+	$("#cardLink").text("+Info Bienal");
+	$("#cardHtml").html("<p>parrafo2</p>");
+	$("#cardContent").text("Los primeros monumentos urbanos(1920) fueron erigidos por la colectividad italiana, los cuales tuvieron el caracter recordatorio de las gestas inmigrantes. A partir de 1961, la historia local comenzo a concretarse con monumentos dedicados a proceres argentinos. En 1961, El Fogon de los Arrieros lanzo un 'Plan de Embellecimiento', donde primaba,en cierta forma crear una 'Ciudad-Museo'. Desde el Primer Concurso de Escultura en Madera en la Plaza Central, en 1988, hasta las Bienales internacionales en la actualidad, El Gobierno de la Provincia del Chaco y la Fundación Urunday trabajan incansablemente para hacer de cada Bienal una verdadera celebración de la identidad de los chaqueños. La Bienal 2016 convoca a once artistas consagrados internacionalmente, representando a distintos paises del mundo, quienes realizaran a cielo abierto, una escultura inspirandoseen el equilibrio. El material a trabajar es el marmol,con el concepto de 'Equilibrium' reconocido como  el cobijo de la armonia, el balance.");
 }
 
 function btn3Click(){
-	$("#cardImage").attr("src", "../static/images/card.png");
-	$("#cardTitle").text("Title 3");
-	$("#cardLink").text("Link 3");
-	$("#cardContent").text("Content 3");
+	$("#cardImage").attr("src", "../static/images/escultores.png");
+	$("#cardTitle").text("Escultores");
+	$("#cardWLink").attr("href", "http://www.bienaldelchaco.org/participantes/");
+	$("#cardLink").text("+Info Escultores");
+	$("#cardHtml").html("<p>parrafo3</p>");
+	$("#cardContent").text("lista de muchachos que laburan");
 }
