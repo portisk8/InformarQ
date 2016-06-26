1895ed33a2b8aaae870eddbf42dc81537ce7986e
Augusto Portillo <Augusto Portillo>
2016-06-26 16:57:16 -0300

    Vistas Home y cronograma

--- static/js/app2.js
+++ static/js/app2.js
@@ -1,73 +1,23 @@
 $(document).ready(function() {
-
-    $('#calendar').fullCalendar({
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-      },
-      defaultDate: '2016-05-12',
-      businessHours: true, // display business hours
-      editable: true,
-      events: [
-        {
-          title: 'Business Lunch',
-          start: '2016-05-03T13:00:00',
-          constraint: 'businessHours',
-          url: 'http://google.com/'
-        },
-        {
-          title: 'Meeting',
-          start: '2016-05-13T11:00:00',
-          constraint: 'availableForMeeting', // defined below
-          color: '#257e4a'
-        },
-        {
-          title: 'Conference',
-          start: '2016-05-18',
-          end: '2016-05-20'
-        },
-        {
-          title: 'Party',
-          start: '2016-05-29T20:00:00'
-        },
-
-        // areas where "Meeting" must be dropped
-        {
-          id: 'availableForMeeting',
-          start: '2016-05-11T10:00:00',
-          end: '2016-05-11T16:00:00',
-          rendering: 'background'
-        },
-        {
-          id: 'availableForMeeting',
-          start: '2016-05-13T10:00:00',
-          end: '2016-05-13T16:00:00',
-          rendering: 'background'
-        },
-
-        // red areas where no events can be dropped
-        {
-          start: '2016-05-24',
-          end: '2016-05-28',
-          overlap: false,
-          rendering: 'background',
-          color: '#ff9f89'
-        },
-        {
-          start: '2016-05-06',
-          end: '2016-05-08',
-          overlap: false,
-          rendering: 'background',
-          color: '#ff9f89'
-        }
-      ],
-      eventClick: function(event) {
-        if (event.url) {
-            window.open(event.url);
-            return false;
-        }
-    }
+  $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
-    
-  });
+});
+
+function clickCalendar() {
+  $(".cronograma.contenido").html('<iframe class="goocalendario" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=ai0mvckkfkv1f074r24tcoalpc%40group.calendar.google.com&amp;color=%2342104A&amp;ctz=America%2FArgentina%2FBuenos_Aires" frameborder="0" scrolling="no"></iframe></div>');
+  $(".cronograma.titulo").text("Actividades Bienal");
+  $(".cronograma.subtitulo").text("Calendario");
+}
+
+function clickDescription(){
+  $(".cronograma.contenido").html('<ul class="collapsible popout" data-collapsible="accordion"><li><div class="collapsible-header"><i class="material-icons">accessibility</i>Escultoricas</div><div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div></li><li><div class="collapsible-header"><i class="material-icons">account_balance</i>Academicas</div><div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div></li><li><div class="collapsible-header"><i class="material-icons">new_releases</i>Muestras</div><div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div></li><li><div class="collapsible-header"><i class="material-icons">assignment_ind</i>Artes Escenicas</div><div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div></li><li><div class="collapsible-header"><i class="material-icons">record_voice_over</i>Artes Combinadas</div><div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div></li><li><div class="collapsible-header"><i class="material-icons">fitness_center</i>Artesanias</div><div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div></li></ul>');
+  $(".cronograma.titulo").text("Description de Actividades Bienal 2016");
+  $(".cronograma.subtitulo").text("Actividades");
+
+  $(document).ready(function() {
+  $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
+}
